c7cdbba81b0b17c860171151ad8511b5bb28b7d1
dinaabdykasheva <dina_abdykasheva@mail.ru>
2017-06-23 22:10:05 +0600

    changes commit

--- src/main/java/pageObject/WriteMailPage.java
+++ src/main/java/pageObject/WriteMailPage.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Dina_Abdykasheva on 6/15/2017.
  */
@@ -16,33 +18,43 @@ public class WriteMailPage extends AbstractPage{
     private static final By BODY_FIELD_LOCATOR = By.xpath(".//div[@role='textbox']");
     private static final By CLOSE_WRITE_MAIL_WINDOW_LOCATOR = By.xpath(".//img[@class='Ha']");
     private static final By SEND_MAIL_BUTTON_LOCATOR = By.xpath(".//div[@class='T-I J-J5-Ji aoO T-I-atl L3']");
-    private static final By DRAFTS_FOLDER_LOCATOR = By.xpath(".//a[contains(text(), '×åðíîâèêè')]");
+    private static final By DRAFTS_FOLDER_LOCATOR = By.xpath(".//a[contains(text(), 'Ð§ÐµÑ€Ð½Ð¾Ð²Ð¸ÐºÐ¸')]");
+    private static final By SAVING_LABEL_LOCATOR = By.xpath(".//span[@class = 'oG aOy']");
+    private static final By TO_FIELD_IN_DRAFT_LOCATOR = By.xpath(".//span[@class='vN bfK a3q']");
+    private static final By SUBJECT_FIELD_IN_DRAFT_LOCATOR = By.name("subjectbox");
+    private static final By BODY_FIELD_IN_DRAFT_LOCATOR = By.xpath(".//div[@role='textbox']");
 
     public WriteMailPage(WebDriver driver) {
         super(driver);
     }
 
     public DraftsFolderPage writeMailAndSaveToDraft(String to, String subject, String body) {
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(TO_FIELD_LOCATOR)).sendKeys(to);
-        //driver.findElement(TO_FIELD_LOCATOR).sendKeys(to);
-        driver.findElement(BODY_FIELD_LOCATOR).sendKeys(subject);
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(TO_FIELD_LOCATOR));
+        actions.click();
+        actions.sendKeys(to);
+        actions.build().perform();
+        driver.findElement(SUBJECT_FIELD_LOCATOR).sendKeys(subject);
         driver.findElement(BODY_FIELD_LOCATOR).sendKeys(body);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(SAVING_LABEL_LOCATOR));
         driver.findElement(CLOSE_WRITE_MAIL_WINDOW_LOCATOR).click();
         driver.findElement(DRAFTS_FOLDER_LOCATOR).click();
         return new DraftsFolderPage(driver);
     }
 
     public String getReceiver() {
-        return driver.findElement(TO_FIELD_LOCATOR).getAttribute("email");
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(TO_FIELD_IN_DRAFT_LOCATOR));
+        return driver.findElement(TO_FIELD_IN_DRAFT_LOCATOR).getAttribute("email");
     }
 
     public String getSubject() {
-        return driver.findElement(SUBJECT_FIELD_LOCATOR).getAttribute("value");
+        return driver.findElement(SUBJECT_FIELD_IN_DRAFT_LOCATOR).getAttribute("value");
     }
 
     public String getBody() {
-        return driver.findElement(BODY_FIELD_LOCATOR).getText();
+        return driver.findElement(BODY_FIELD_IN_DRAFT_LOCATOR).getText();
     }
 
     public AccountPage sendMail() {
